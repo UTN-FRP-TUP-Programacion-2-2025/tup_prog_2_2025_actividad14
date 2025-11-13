dd7354eb30783a7a205f0e4379ece66482d2205f
fernandofilipuzzi-dev <fernandofilipuzzi.dev@gmail.com>
2025-11-13 16:40:04 -0300

    actualizacion - diseño formulario y modelo

--- Actividad14_/Ejercicio1_Models/Sistema.cs
+++ Actividad14_/Ejercicio1_Models/Sistema.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
-namespace Ejercicio1_Models
+namespace Ejercicio1_Models;
+
+public class Sistema
 {
-    internal class Sistema
+    List<Ticket> tickets=new List<Ticket>();
+    List<Transporte> transportes=new List<Transporte>();
+
+    public void AgregarTransporte(Transporte transporte)
     {
+        transportes.Add(transporte);
+    }
+
+    public void CrearTicket(string cuit, string nombre, string telefono, string tarjeta)
+    {
+        Ticket t=new Ticket(cuit, nombre, telefono, tarjeta);
+        tickets.Add(t);
+    }
+
+    public string[] VerTickets()
+    {
+        string[] lista = new string[tickets.Count];
+
+        tickets.Sort();
+
+        int n = 0;
+        foreach (Ticket t in tickets)
+        {
+            lista[n++] = t.ToString();
+        }
+
+        return lista;
     }
 }
---
+++ Actividad14_/Ejercicio2_Desktop/FormPrincipal.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace Ejercicio2_Desktop
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            textBox1 = new TextBox();
+            tbCuit = new TextBox();
+            tbNombre = new TextBox();
+            tbTelefono = new TextBox();
+            tbTarjeta = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            groupBox1 = new GroupBox();
+            btnGenerar = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(126, 12);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 0;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(126, 70);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(121, 23);
+            comboBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(126, 41);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // tbCuit
+            // 
+            tbCuit.Location = new Point(106, 31);
+            tbCuit.Name = "tbCuit";
+            tbCuit.Size = new Size(100, 23);
+            tbCuit.TabIndex = 3;
+            // 
+            // tbNombre
+            // 
+            tbNombre.Location = new Point(106, 60);
+            tbNombre.Name = "tbNombre";
+            tbNombre.Size = new Size(100, 23);
+            tbNombre.TabIndex = 4;
+            // 
+            // tbTelefono
+            // 
+            tbTelefono.Location = new Point(106, 89);
+            tbTelefono.Name = "tbTelefono";
+            tbTelefono.Size = new Size(100, 23);
+            tbTelefono.TabIndex = 5;
+            // 
+            // tbTarjeta
+            // 
+            tbTarjeta.Location = new Point(106, 118);
+            tbTarjeta.Name = "tbTarjeta";
+            tbTarjeta.Size = new Size(100, 23);
+            tbTarjeta.TabIndex = 6;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(36, 28);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 8;
+            label1.Text = "label1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(39, 49);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 9;
+            label2.Text = "label2";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(36, 78);
+            label3.Name = "label3";
+            label3.Size = new Size(38, 15);
+            label3.TabIndex = 10;
+            label3.Text = "label3";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(41, 31);
+            label4.Name = "label4";
+            label4.Size = new Size(38, 15);
+            label4.TabIndex = 11;
+            label4.Text = "label4";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(36, 63);
+            label5.Name = "label5";
+            label5.Size = new Size(38, 15);
+            label5.TabIndex = 12;
+            label5.Text = "label5";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(36, 92);
+            label6.Name = "label6";
+            label6.Size = new Size(38, 15);
+            label6.TabIndex = 13;
+            label6.Text = "label6";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(33, 121);
+            label7.Name = "label7";
+            label7.Size = new Size(38, 15);
+            label7.TabIndex = 14;
+            label7.Text = "label7";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(tbCuit);
+            groupBox1.Controls.Add(tbNombre);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(tbTelefono);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(tbTarjeta);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Location = new Point(12, 99);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(353, 147);
+            groupBox1.TabIndex = 16;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // btnGenerar
+            // 
+            btnGenerar.Location = new Point(270, 11);
+            btnGenerar.Name = "btnGenerar";
+            btnGenerar.Size = new Size(75, 43);
+            btnGenerar.TabIndex = 17;
+            btnGenerar.Text = "Generar";
+            btnGenerar.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(270, 60);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 18;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(231, 252);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 19;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(53, 252);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 20;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // FormPrincipal
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(388, 286);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(btnGenerar);
+            Controls.Add(groupBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(comboBox2);
+            Controls.Add(comboBox1);
+            Name = "FormPrincipal";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private TextBox textBox1;
+        private TextBox tbCuit;
+        private TextBox tbNombre;
+        private TextBox tbTelefono;
+        private TextBox tbTarjeta;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private GroupBox groupBox1;
+        private Button btnGenerar;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+    }
+}
